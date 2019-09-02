b2b4640d7e8eab962d9156f38b51d208ace6f0dc
phantom3518 <1085540763@qq.com>
2019-09-03 01:01:24 +0800

    mintui

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <!--header-->
+    <div class="header">
+      <mt-header fixed title="黑马程序员-Vue项目"></mt-header>
     </div>
-    <router-view/>
+    <!--router-view-->
+    <!--tabbar-->
+    <nav class="mui-bar mui-bar-tab">
+      <a class="mui-tab-item mui-active" href="#tabbar">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-chat">
+        <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+        <span class="mui-tab-label">消息</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-contact">
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">通讯录</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-map">
+        <span class="mui-icon mui-icon-gear"></span>
+        <span class="mui-tab-label">设置</span>
+      </a>
+    </nav>
+    <!---->
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  #app {
+    padding-top: 40px;
   }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import { Header } from 'mint-ui'
+import '../src/assets/mui/css/mui.min.css'
+Vue.component(Header.name, Header)
+// Vue.use(MintUI)
 
+// import { Button } from 'mint-ui'
+// Vue.component(Button.name, Button)
 Vue.config.productionTip = false
 
 new Vue({
